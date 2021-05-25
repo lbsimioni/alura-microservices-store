33f9999792a146a8c9d2d341b30ccd7118f3fe19
Leonardo Bucioli Simioni <lsimioni@ciandt.com>
2021-05-24 22:13:48 -0300

    updates

--- src/main/java/br/com/microservices/store/services/PurchaseService.java
+++ src/main/java/br/com/microservices/store/services/PurchaseService.java
@@ -9,7 +9,6 @@ import lombok.AccessLevel;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Setter(AccessLevel.PRIVATE)
@@ -17,20 +16,10 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class PurchaseService {
 
-//    @Autowired
-//    private RestTemplate client;
-
-//    @Value("${microservices.provider.url}")
-//    private String providerUrl;
-
     @Autowired
     private ProviderClient providerClient;
 
     public Purchase execute(PurchaseRequestDTO requestDTO) {
-        // Implementation with RestTemplate
-//        var exchange = client.exchange(
-//                String.format("%s/info/%s", providerUrl, requestDTO.getAddress().getState()),
-//                HttpMethod.GET, null, ProviderInfoDTO.class);
 
         ProviderInfoDTO info = providerClient.getInfoByState(requestDTO.getAddress().getState());
         log.info("Provider information: " + info);
